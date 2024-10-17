fda9d2d39272436313d38c31366ca894437385ea
Anders Windelhed <anders.windelhed@tv4.se>
2024-10-17 15:42:56 +0200

    Adding null check

--- src/format_email.ts
+++ src/format_email.ts
@@ -1,6 +1,8 @@
 import type dayjs from "dayjs";
 
 const formatEmail = (pattern: string, date: dayjs.Dayjs): string => {
+  if (!pattern) return '';
+
   const datePattern = /\[(.*?)\]/g;
   return pattern.replace(datePattern, (_, format) => date.format(format));
 }
